c26c7ac89dd50fc2cb38c4e971cf0372cedfed4c
nanom1t <nanom1t@mail.ru>
2017-10-18 12:59:53 +0300

    Method prototypes

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 'use strict';
-
 var shardUUID = require('./build/Release/shard-uuid.node');
+var Long = require('long');
 
-console.log(shardUUID.Hello());
+/**
+ * UUID (64bits) = timestamp (32 bits) + shardId (16 bits) + localId (16 bits)
+ *
+ */
+
+function paramDefault(param, defaultValue) {
+    return param == undefined ? defaultValue : param;
+}
+
+function unLittleEndian(buf) {
+    for (var i=0;i<4;i++) {
+        buf[i] ^= buf[7-i];
+        buf[7-i] ^= buf[i];
+        buf[i] ^= buf[7-i];
+    }
+    return buf;
+}
+
+function toLong(buf) {
+    return new Long(buf.readInt32LE(0), buf.readInt32LE(4), true);
+}
+
+var uuid = shardUUID.GetUUID();
+var time = shardUUID.GetTime();
+var shardId = shardUUID.GetShardId();
+var localId = shardUUID.GetLocalId();
+
+console.log(toLong(uuid).toString());
+console.log(toLong(time).toString());
+console.log(toLong(shardId).toString());
+console.log(toLong(localId).toString());
--- shard-uuid.cc
+++ shard-uuid.cc
@@ -12,15 +12,82 @@ using Nan::CopyBuffer;
 using Nan::New;
 using Nan::Set;
 
-NAN_METHOD(Hello) {
-    // Create an instance of V8's String type
-    auto message = Nan::New("Hello from C++!").ToLocalChecked();
+#define EPOCH 1483228800L // 2017-01-01 00:00:00
 
-    info.GetReturnValue().Set(message);
+typedef unsigned long long UINT64;
+typedef unsigned long UINT32;
+
+UINT64 getTime() {
+    auto now = std::chrono::system_clock::now().time_since_epoch().count() / 1000;
+    UINT64 time = (UINT64)(now / 1000);
+
+    return time;
 }
 
+UINT64 getShardId() {
+    return sizeof(UINT64);
+}
+
+UINT64 getLocalId() {
+    return getTime() - EPOCH;
+}
+
+UINT64 getUUID(UINT32 shardId, UINT32 localId) {
+    UINT64 id = 0;//getTime();// << (64-41);
+
+    // aditional number (13 bit : 0~8191) - use microseconds if not exist
+    //id |= (additional != 0 ? additional : now % 1000) << (64-41-13);
+
+    // counter number (10 bit : 0~1023)
+    //id |= counter % 1024;
+
+    return id;
+}
+
+/**
+ * Method wrapping for node export
+ */
+
+NAN_METHOD(GetUUID) {
+    UINT64 uuid = getUUID(0, 0);
+
+    // pass it to buffer
+    auto buffer = CopyBuffer((char*)(&uuid), 8);
+    info.GetReturnValue().Set(buffer.ToLocalChecked());
+}
+
+NAN_METHOD(GetTime) {
+    UINT64 time = getTime();
+
+    // pass it to buffer
+    auto buffer = CopyBuffer((char*)(&time), 8);
+    info.GetReturnValue().Set(buffer.ToLocalChecked());
+}
+
+NAN_METHOD(GetShardId) {
+    UINT64 shardId = getShardId();
+
+    // pass it to buffer
+    auto buffer = CopyBuffer((char*)(&shardId), 8);
+    info.GetReturnValue().Set(buffer.ToLocalChecked());
+}
+
+NAN_METHOD(GetLocalId) {
+    UINT64 localId = getLocalId();
+
+    // pass it to buffer
+    auto buffer = CopyBuffer((char*)(&localId), 8);
+    info.GetReturnValue().Set(buffer.ToLocalChecked());
+}
+
+/**
+ * Initialize node module
+ */
 NAN_MODULE_INIT(Initialize) {
-    NAN_EXPORT(target, Hello);
+    NAN_EXPORT(target, GetUUID);
+    NAN_EXPORT(target, GetTime);
+    NAN_EXPORT(target, GetShardId);
+    NAN_EXPORT(target, GetLocalId);
 }
 
 NODE_MODULE(shard_uuid, Initialize);
